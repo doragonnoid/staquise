c4a6fd24ed4fd82ecdbbdab7d6adb6b066bc0e3b
doragonnoid <gunandodwii@gmail.com>
2025-02-11 10:55:13 +0700

    Update new.py

--- new.py
+++ new.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import spectrogram
 
 # Load CSV file without header
 df = pd.read_csv("selamat.csv", header=None)
